0c901efe487fe317aab44e01d0e0914e319e4d29
Bùi Minh Tiến <TienBM@Admins-MacBook-Pro.local>
2016-12-30 21:42:55 +0700

    Update

---
+++ Thu/Thu/aaaaViewController.swift
@@ -0,0 +1,36 @@
+//
+//  aaaaViewController.swift
+//  Thu
+//
+//  Created by Bùi Minh Tiến on 12/30/16.
+//  Copyright © 2016 TienBM. All rights reserved.
+//
+
+import UIKit
+
+class aaaaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Thieu Mao")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
